90459ad149264119a42acb958780a6879ad99ec4
kostt <felix-mazur@mail.ru>
2017-09-24 16:50:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<html>
 <head>
-<title>Ареол</title>
+<title>Ариол</title>
 <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
 </head>
 <body>
